a7391733bd046fee6f02abb7f74abe25535be3a1
hernan <hernan-vilar@hotmail.com>
2021-04-29 20:27:22 -0300

    faltamemotestysprint5

--- src/app/Juegos/page/piedra-papelo-tijera/piedra-papelo-tijera.component.ts
+++ src/app/Juegos/page/piedra-papelo-tijera/piedra-papelo-tijera.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MensajesRealtimeService } from 'src/app/services/mensajes-realtime.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-piedra-papelo-tijera',
@@ -8,12 +9,122 @@ import { MensajesRealtimeService } from 'src/app/services/mensajes-realtime.serv
 })
 export class PiedraPapeloTijeraComponent implements OnInit {
 
-  constructor(private cambiarmensajereal:MensajesRealtimeService)
+  constructor(private cambiarmensajereal:MensajesRealtimeService,private toastr:ToastrService)
   {
     this.cambiarmensajereal.dbPath = "/MensajesPiedraPapelTijera";
   }
 
   ngOnInit(): void {
   }
+   ROCK = "rock";
+   PAPER = "paper";
+   SCISSORS = "scissors";
+   variante:any;
+  resultText:any;
+  aca:any;
+  aca2:any;
+  
+   TIE:number = 0;
+   WIN:number = 1;
+   LOST:number = 2;
+  
+  isPlaying:boolean = false;
+  
+  clickpiedra()
+  {
+    this.play(this.ROCK);
+  }
+clickpapel()
+{
+    this.play(this.PAPER);
+}
+clicktijera()
+{
+    this.play(this.SCISSORS);
+}
+
+
+ play(userOption:string) {
+    if(this.isPlaying) return;
+
+    this.isPlaying = true;
+
+    this.aca = "../../../../assets/imagenes/" + userOption + ".svg";
+
+    this.resultText = "Chossing!";
+
+    const interval = setInterval(()=>{
+        const machineOption = this.calcMachineOption();
+        this.aca2 = "../../../../assets/imagenes/" + machineOption + ".svg";
+    }, 200);
+
+    setTimeout( () => {
+
+        clearInterval(interval);
+
+        const machineOption = this.calcMachineOption();
+        const result = this.calcResult(userOption, machineOption);
+
+        this.aca2= "../../../../assets/imagenes/" + machineOption + ".svg";
+
+        switch (result) {
+            case this.TIE:
+                this.variante =  "EMPATASTE!!";
+                this.toastr.warning('CASIIIIIII AHI NOMAS....):','VAMOS, TU PUEDES CASI LE GANASSSS!',{
+                    timeOut:3000
+                });
+                break;
+            case this.WIN:
+                this.variante= "GANASTE!!";
+                this.toastr.success('GANASTE SOS UN CRACK!!.:)','VAMOS, GENIOOOOOOOOOO!!!!!!!!!!!',{
+                    timeOut:3000
+                });
+                break;
+            case this.LOST:
+                this.variante = "PERDISTE!!"
+                this.toastr.error('UPSSSSSSSSS! QUE LASTIMA PERO PERDISTE.):','SEGUI PRACTICANDO MASS',{
+                    timeOut:3000
+                });
+                break;
+        }
+        this.isPlaying = false;
+    }, 2000);
+}
+
+ calcMachineOption() {
+    const number = Math.floor(Math.random() * 3);
+    switch (number) {
+        case 0:
+            return this.ROCK;
+        case 1:
+            return this.PAPER;
+        case 2:
+            return this.SCISSORS;
+    }
+    return "";
+}
+
+ calcResult(userOption:any, machineOption:any) {
+    if (userOption === machineOption) {
+        return this.TIE;
+
+    } else if (userOption === this.ROCK) {
+
+        if (machineOption === this.PAPER) return this.LOST;
+        if (machineOption === this.SCISSORS) return this.WIN;
+
+    } else if (userOption === this.PAPER) {
+
+        if (machineOption === this.SCISSORS) return this.LOST;
+        if (machineOption === this.ROCK) return this.WIN;
+
+    } else if (userOption === this.SCISSORS) {
+
+        if (machineOption === this.ROCK) return this.LOST;
+        if (machineOption === this.PAPER) return this.WIN;
+
+    }
+    return "";
+}
 
 }
--- src/app/Juegos/page/tateti/tateti.component.ts
+++ src/app/Juegos/page/tateti/tateti.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MensajesRealtimeService } from 'src/app/services/mensajes-realtime.service';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-tateti',
@@ -8,12 +11,135 @@ import { MensajesRealtimeService } from 'src/app/services/mensajes-realtime.serv
 })
 export class TatetiComponent implements OnInit {
 
-  constructor(private cambiarmensajereal:MensajesRealtimeService)
-  {
-     this.cambiarmensajereal.dbPath = "/MensajesTateti";
-  }
+   comenzar:boolean = false;
+  casilleros = new Array(9);
+  ficha :string = "X";
+
+  constructor(private toastr:ToastrService) {
+   }
 
   ngOnInit(): void {
+  }
   
-}
+  jugar(numeroCasillero:number){
+    
+    if(!this.casilleros[numeroCasillero]){
+      this.casilleros[numeroCasillero] = this.ficha;
+      var celdas = document.getElementsByClassName("celda");
+      for (let index = 0; index < celdas.length; index++) {
+        if(index == numeroCasillero){
+          celdas[index].setAttribute("value",this.ficha.toUpperCase());
+          break;
+        }
+        
+      }
+      
+      if(this.gano(this.ficha)){
+        console.log("GANASTE");
+        this.toastr.success('GANASTE QUE BIENN!!!!.:','SOS UN GROSO..!',{
+          timeOut:2000
+        });
+        this.reiniciar();
+      }else if(this.empate()){
+
+        this.toastr.warning('Empantaste!!!!.:','UPSS, QUE PENA..!',{
+          timeOut:2000
+        });
+        this.reiniciar();
+      }
+      else{
+        setTimeout(() => this.juegaMaquina(), 500);
+      }
+    }
+  }
+
+  reiniciar(){
+    var celdas = document.getElementsByClassName("celda");
+      for (let index = 0; index < celdas.length; index++) {
+        
+          celdas[index].setAttribute("value",'');
+          
+        }
+    this.casilleros = new Array(9);
+    
+  }
+  
+  empate():boolean{
+    let retorno = true;
+    for (let index = 0; index < this.casilleros.length; index++) {
+      if(!this.casilleros[index]){
+        retorno = false;
+        break;
+      }
+    }
+    return retorno;
+  }
+  
+  juegaMaquina(){
+    console.log("JUEGA MAQUINA");
+    
+    let disponible = false;
+    let numCasillero:number = 0;
+    while(!disponible){
+      numCasillero = Math.floor(Math.random() * 9 + 0);
+      if(!this.casilleros[numCasillero]){
+        disponible = true;
+      }
+    }
+    var fichaMaquina = "O";
+    this.casilleros[numCasillero] = fichaMaquina;
+    var celdas = document.getElementsByClassName("celda");
+      for (let index = 0; index < celdas.length; index++) {
+        if(index == numCasillero){
+          celdas[index].setAttribute("value",fichaMaquina.toUpperCase());
+          break;
+        }
+        
+      }
+
+      if(this.gano(fichaMaquina)){
+        console.log("GANA LA MAQUINA");
+        this.toastr.warning('UUUPSSS!!!!.:','UPSS, QUE PENA PERO PERDISTE..!',{
+          timeOut: 2000
+        });
+        this.reiniciar();
+      }else if(this.empate()){
+        this.reiniciar();
+      }
+  } 
+
+  gano(letra:string):boolean {
+    
+    if(this.casilleros[0] == letra && this.casilleros[1] == letra && this.casilleros[2] == letra) {
+      return true;
+    }
+    if(this.casilleros[3] == letra && this.casilleros[4] == letra && this.casilleros[5] == letra) {
+      return true;
+    }
+    if(this.casilleros[6] == letra && this.casilleros[7] == letra && this.casilleros[8] == letra) {
+      return true;
+    }
+
+    if(this.casilleros[0] == letra && this.casilleros[3] == letra && this.casilleros[6] == letra) {
+      return true;
+    }
+    if(this.casilleros[1] == letra && this.casilleros[4] == letra && this.casilleros[7] == letra) {
+      return true;
+    }
+    if(this.casilleros[2] == letra && this.casilleros[5] == letra && this.casilleros[8] == letra) {
+      return true;
+    }
+
+    if(this.casilleros[0] == letra && this.casilleros[4] == letra && this.casilleros[8] == letra) {
+      return true;
+    }
+    if(this.casilleros[2] == letra && this.casilleros[4] == letra && this.casilleros[6] == letra) {
+      return true;
+    }
+    return false;
+  }
+
+  eligeFicha(ficha:string){
+    this.ficha = ficha;
+  }
 }
